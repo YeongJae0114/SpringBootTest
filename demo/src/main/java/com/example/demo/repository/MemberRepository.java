5a51001baee1ea03bb2b2fac7083e86db9f743db
YeongJae0114 <yjl8628@naver.com>
2023-12-18 17:32:33 +0900

    :sparkles: Repository
    데이터베이스 접근, domain 객체 DB에 저장 관리

--- demo/src/main/java/com/example/demo/repository/MemberRepository.java
+++ demo/src/main/java/com/example/demo/repository/MemberRepository.java
@@ -1,2 +1,16 @@
-package com.example.demo.repository;public interface MemberRepository {
+package com.example.demo.repository;
+
+import com.example.demo.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+
+
 }
